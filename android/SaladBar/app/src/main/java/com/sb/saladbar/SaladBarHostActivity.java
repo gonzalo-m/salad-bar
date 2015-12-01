be23f3bbafe8c7786f8963efa07410e0eca09847
Gonzalo <gmartz21@gmail.com>
2015-12-01 13:59:07 -0500

    Action bar works to go back and forth between fragments

--- android/SaladBar/app/src/main/java/com/sb/saladbar/SaladBarHostActivity.java
+++ android/SaladBar/app/src/main/java/com/sb/saladbar/SaladBarHostActivity.java
@@ -1,9 +1,11 @@
 package com.sb.saladbar;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class SaladBarHostActivity extends AppCompatActivity {
@@ -13,13 +15,14 @@ public class SaladBarHostActivity extends AppCompatActivity {
     private SaladBarFragment mSaladBarFragment = new SaladBarFragment();
     private PlaceOrderFragment mPlaceOrderFragment = new PlaceOrderFragment();
 
+    private MenuItem mToggleMenuButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_salad_bar_host);
-//
+
         setTitle("Sweetgreen");
-        getSupportActionBar().setIcon(R.drawable.ic_drawer);
 
         // add to host activity
         getSupportFragmentManager()
@@ -32,7 +35,9 @@ public class SaladBarHostActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_salad_bar_host, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_salad_bar_host, menu);
+        mToggleMenuButton = menu.findItem(R.id.action_add_order);
         return true;
     }
 
@@ -46,28 +51,46 @@ public class SaladBarHostActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_add_order) {
             Log.i(TAG, "adding order");
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            showNextFragment(null);
+            Fragment currFragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+            if (currFragment != null && currFragment.isVisible()) {
+                if (currFragment instanceof SaladBarFragment) {
+                    Log.i(TAG, "curr: salad");
+                    showNextFragment();
+                } else if (currFragment instanceof  PlaceOrderFragment ){
+                    Log.i(TAG, "curr: order");
+                    showPreviousFragment();
+                }
+            }
+
+
             return true;
+
         } else if (id == android.R.id.home) {
             Log.i(TAG, "back");
-            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-            getSupportFragmentManager().popBackStack();
+            showPreviousFragment();
         }
 
         return super.onOptionsItemSelected(item);
     }
 
 
-    public void showNextFragment(String backStackName) {
+    public void showNextFragment() {
+        mToggleMenuButton.setIcon(android.R.drawable.ic_input_add);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportFragmentManager()
                 .beginTransaction()
                 .setCustomAnimations(R.anim.slide_right_enter, R.anim.slide_left_exit,
                         R.anim.slide_left_enter, R.anim.slide_right_exit)
                 .replace(R.id.fragment_container, mPlaceOrderFragment)
-                .addToBackStack(backStackName)
+                .addToBackStack(null)
                 .commit();
         getFragmentManager().executePendingTransactions();
+    }
+
+    public void showPreviousFragment() {
+        mToggleMenuButton.setIcon(R.drawable.paperbag_brown);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        getSupportFragmentManager().popBackStack();
     }
 
 }
