a62fb64a1bd5a77889c6f2f93d5962166c33c499
maryallisonabad <maryallisonabad@gmail.com>
2015-12-03 10:22:05 -0500

    Modified ingredient prices to be more realistic

--- android/SaladBar/app/src/main/java/com/sb/saladbar/model/ingredients/Premium.java
+++ android/SaladBar/app/src/main/java/com/sb/saladbar/model/ingredients/Premium.java
@@ -7,15 +7,15 @@ import com.sb.saladbar.R;
  */
 public enum Premium implements Ingredient {
 
-    AVOCADO("Avocado", .25, 140, R.drawable.premium_avocado),
-    BACON("Bacon", .25, 170, R.drawable.premium_bacon),
-    GOAT_CHEESE("Goat Cheese", .25, 80, R.drawable.premium_goat_cheese),
-    HARD_BOILED_EGG("Egg", .25, 80, R.drawable.premium_hard_boiled_egg),
-    HOUSEMADE_HUMMUS("Hummus", .25, 90, R.drawable.premium_housemade_hummus),
-    ORGANIC_WHITE_CHEDDAR("White Cheddar", .25, 130, R.drawable.premium_organic_white_cheddar),
-    ROASTED_CHICKEN("Chicken", 25, 140, R.drawable.premium_roasted_chicken),
-    ROASTED_CURRY_CAULIFLOWER("Cauliflower", .25, 30, R.drawable.premium_roasted_curry_cauliflower),
-    SHAVED_PARMESAN("Parmesan", .25, 45, R.drawable.premium_shaved_parmesan);
+    AVOCADO("Avocado", 2.25, 140, R.drawable.premium_avocado),
+    BACON("Bacon", 2.00, 170, R.drawable.premium_bacon),
+    GOAT_CHEESE("Goat Cheese", 1.75, 80, R.drawable.premium_goat_cheese),
+    HARD_BOILED_EGG("Egg", 2.00, 80, R.drawable.premium_hard_boiled_egg),
+    HOUSEMADE_HUMMUS("Hummus", 2.25, 90, R.drawable.premium_housemade_hummus),
+    ORGANIC_WHITE_CHEDDAR("White Cheddar", 1.75, 130, R.drawable.premium_organic_white_cheddar),
+    ROASTED_CHICKEN("Chicken", 2.50, 140, R.drawable.premium_roasted_chicken),
+    ROASTED_CURRY_CAULIFLOWER("Cauliflower", 2.25, 30, R.drawable.premium_roasted_curry_cauliflower),
+    SHAVED_PARMESAN("Parmesan", 1.75, 45, R.drawable.premium_shaved_parmesan);
 
 
     private String name;
