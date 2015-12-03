dfb3a75815de07674869705b2b8f129281c09968
Gonzalo <gmartz21@gmail.com>
2015-12-03 10:33:05 -0500

    Header and footer done

--- android/SaladBar/app/src/main/java/com/sb/saladbar/OrderConfirmationActivity.java
+++ android/SaladBar/app/src/main/java/com/sb/saladbar/OrderConfirmationActivity.java
@@ -16,27 +16,46 @@ public class OrderConfirmationActivity extends AppCompatActivity {
 
     public static final String TAG = OrderConfirmation.class.getSimpleName();
 
-    private final static SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("MMM-dd-yyyy", Locale.US);
-    private final static SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("h:mma", Locale.US);
 
-    TextView dateTextView, timeTextView;
+
+    private TextView mDateTextView, mTimeTextView, mConfirmationNumTextView, mTotalView;
+    private OrderConfirmation mOrderConfirmation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_confirmation);
 
-        //TODO: (Aldene) here is the orderConfirmation ready to be used
-        OrderConfirmation orderConfirmation = (OrderConfirmation) getIntent().getSerializableExtra(Intent.EXTRA_INTENT);
-        Log.i(TAG, orderConfirmation.toString());
+        //TODO: here is the orderConfirmation ready to be used
+        mOrderConfirmation = (OrderConfirmation) getIntent().getSerializableExtra(Intent.EXTRA_INTENT);
+        Log.i(TAG, mOrderConfirmation.toString());
 
-        Calendar cal = Calendar.getInstance();
-        String dateText = DATE_FORMAT.format(cal.getTime());
-        String timeText = TIME_FORMAT.format(cal.getTime());
+        mDateTextView = (TextView) findViewById(R.id.textView_order_date);
+        mTimeTextView = (TextView) findViewById(R.id.textView_order_time);
+        mConfirmationNumTextView = (TextView) findViewById(R.id.textView_confirmation_number);
+        mTotalView = (TextView) findViewById(R.id.textView_order_total_amount);
 
-        dateTextView = (TextView) findViewById(R.id.textView_order_date);
-        timeTextView = (TextView) findViewById(R.id.textView_order_time);
 
-        dateTextView.setText(dateText);
-        timeTextView.setText(timeText.toLowerCase());
+        updateHeaderView();
+        updateFooterView();
+        updateItemViews();
+    }
+
+    private void updateHeaderView() {
+        String orderNum = mOrderConfirmation.getOrderNum();
+        String date = mOrderConfirmation.getConfirmationDate();
+        String time = mOrderConfirmation.getConfirmationTime();
+        mConfirmationNumTextView.setText(orderNum);
+        mTimeTextView.setText(time);
+        mDateTextView.setText(date);
+    }
+
+    private void updateFooterView() {
+
+    }
+
+    private void updateItemViews() {
+        String total = mOrderConfirmation.getTotal();
+        mTotalView.setText(total);
     }
 }
--- android/SaladBar/app/src/main/java/com/sb/saladbar/model/OrderConfirmation.java
+++ android/SaladBar/app/src/main/java/com/sb/saladbar/model/OrderConfirmation.java
@@ -1,36 +1,55 @@
 package com.sb.saladbar.model;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by G on 11/23/15.
  */
 public class OrderConfirmation implements Serializable {
 
+    private final static SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("MMM-dd-yyyy", Locale.US);
+    private final static SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("h:mma", Locale.US);
+
     private static int orderIds = 1;
 
-    private final int orderNum;
-    private final Calendar confirmationDate;
+    private final String orderNum;
+    private final String date;
+    private final String time;
+    private final String total;
     private final Order orderPlaced;
 
     public OrderConfirmation(Order orderPlaced) {
-        orderNum = orderIds++;
-        confirmationDate = Calendar.getInstance();
+        orderNum = String.valueOf(orderIds++);
+        Calendar cal = Calendar.getInstance();
+        date = DATE_FORMAT.format(cal.getTime());
+        time = TIME_FORMAT.format(cal.getTime());
+        total = String.valueOf(orderPlaced.getTotal());
+
         this.orderPlaced = orderPlaced;
     }
 
-    public int getOrderNum() {
+    public String getOrderNum() {
         return orderNum;
     }
 
-    public Calendar getConfirmationDate() {
-        return confirmationDate;
+    public String getConfirmationTime() {
+        return time;
+    }
+
+    public String getConfirmationDate() {
+        return date;
     }
 
     public Order getOrderPlaced() {
         return orderPlaced;
+    }
+
+    public String getTotal() {
+        return total;
     }
 
 }
