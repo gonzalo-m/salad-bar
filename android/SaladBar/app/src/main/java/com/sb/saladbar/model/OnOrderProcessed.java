aafb7cac36b3be79da78e4344a873499271f64e5
Jose <joseph_wu15@yahoo.com>
2015-12-02 21:41:33 -0500

    updated onOrderProcessed

--- android/SaladBar/app/src/main/java/com/sb/saladbar/model/OnOrderProcessed.java
+++ android/SaladBar/app/src/main/java/com/sb/saladbar/model/OnOrderProcessed.java
@@ -1,11 +1,46 @@
 package com.sb.saladbar.model;
 
-/**
- * Created by G on 11/23/15.
- */
-public interface OnOrderProcessed {
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
 
-    void onOnOrderConfirmation(OrderConfirmation orderConfirmation);
+import com.sb.saladbar.R;
 
-    void onOrderReady(OrderConfirmation orderConfirmation);
+
+public class OnOrderProcessed {
+    static NotificationManager mNotificationManager;
+    private static Context mContext;
+
+    public OnOrderProcessed(Context context) {
+        mContext = context;
+    }
+
+    public static void onOnOrderConfirmation(OrderConfirmation orderConfirmation){
+        //set loading screen to simulate salad ordering request?
+        Toast.makeText(mContext, "Order Confirmed", Toast.LENGTH_LONG).show();
+    }
+
+    public static void onOrderReady(OrderConfirmation orderConfirmation) {
+        //Send notification to notification area
+        mNotificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+        CharSequence title = "SweetGreen";
+        CharSequence text = "Your Order Is Ready";
+        Notification.Builder orderReadyNotification = new Notification.Builder(mContext)
+                .setContentText(text)
+                .setContentTitle(text)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.green2)
+                .setOngoing(true)
+                .setAutoCancel(true);
+        PendingIntent mContentIntent = PendingIntent.getActivity(mContext, 0, new Intent(), 0);
+        orderReadyNotification.setContentIntent(mContentIntent);
+        mNotificationManager.notify(0, orderReadyNotification.build());
+
+        //Send Toast notification
+        Toast.makeText(mContext, "Your Order Is Ready", Toast.LENGTH_LONG).show();
+    }
 }
+
